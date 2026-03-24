c73d72ca9dbd829b51cfd204e5238f5b2fc5eb44
SherifaSayed <sherifasayedabrahem@gmail.com>
2026-03-24 10:03:50 +0200

    asymmetric metod

--- Src/Utils/encryption.utils.js
+++ Src/Utils/encryption.utils.js
@@ -1,5 +1,7 @@
-import crypto from "node:crypto"
+import crypto, { privateDecrypt, publicEncrypt } from "node:crypto"
 import envConfig from '../config/env.config.js'
+import fs from'node:fs'
+import { buffer } from "node:stream/consumers";
 const encryptionENV= envConfig.encription;
 const encryKey= Buffer.from(encryptionENV.ENCRIPTION_KEY,'hex')
 //================ symmetric
@@ -24,4 +26,46 @@ export const decrypt=(inputCipher)=>{
 
     return decrypted;
 
+}
+if(fs.existsSync('publicKey.pem')&&fs.existsSync('privateKey.pem'))
+{
+    console.log("private and public keys genertated")
+}
+else{
+const {publicKey, privateKey}=crypto.generateKeyPairSync('rsa',{modulusLength:2084,
+    publicKeyEncoding:{
+        type:'pkcs1',
+        format:'pem'
+    }, 
+    privateKeyEncoding:{
+        type:'pkcs1',
+        format:'pem'
+    }
+})
+fs.writeFileSync('publicKey.pem',publicKey)
+fs.writeFileSync('privateKey.pem',privateKey)
+
+}
+export const asymmetricEncryption=(text)=>{
+    const publicKey=fs.readFileSync('publicKey.pem','utf-8');
+    const bufferdText= Buffer.from(text);
+    const encryptedData= crypto.publicEncrypt({
+        key:publicKey,
+        padding:crypto.constants.RSA_PKCS1_OAEP_PADDING
+      },
+      bufferdText
+    )
+ return  encryptedData.toString('hex');
+}
+
+export const asymmertricDecryption=(text)=>{
+    const privateKey=fs.readFileSync('publicKey.pem','utf-8');
+    const bufferdText= Buffer.from(text,'hex');
+    const decryptedData= crypto.privateDecrypt({
+        key:privateKey,
+        padding:crypto.constants.RSA_PKCS1_OAEP_PADDING 
+      },
+      bufferdText
+    )
+ return  decryptedData.toString('utf-8');
 }
